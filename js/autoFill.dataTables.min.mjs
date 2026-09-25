3a2dfe606832c22b8da5b9d6fa62da4f7c4f1673
dtbuild <dtbuild@rigil>
2026-09-25 10:55:47 +0000

    a4d18c4a57d827d8aa91c3e61aa10dc3453a856b Release 3.1.2
    Sync to source repo @a4d18c4a57d827d8aa91c3e61aa10dc3453a856b

--- js/autoFill.dataTables.min.mjs
+++ js/autoFill.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! AutoFill default styling 3.1.1 for DataTables
+/*! AutoFill default styling 3.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import AutoFill from"datatables.net-autofill";export default DataTable;
